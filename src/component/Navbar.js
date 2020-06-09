41ddc630e113c4fc4b17979cf6c4e6b8d1626b05
barthelemy de violet <barthelemy.fornier-de-violet@epitech.eu>
2020-06-09 16:20:40 +0200

    Initail commit of react-app with material-ui + navbar + toggle menu

---
+++ src/component/Navbar.js
@@ -0,0 +1,135 @@
+import React, { Fragment, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import MobilRightMenuSlider from "@material-ui/core/Drawer";
+import {
+  AppBar,
+  Toolbar,
+  ListItem,
+  IconButton,
+  ListItemText,
+  Avatar,
+  Divider,
+  List,
+  Typography,
+  Box,
+  ListItemIcon,
+} from "@material-ui/core";
+import {
+  Menu,
+  MenuBook,
+  ArrowBack,
+  AssignmentInd,
+  Home,
+  Apps,
+  ContactMail,
+  GitHub,
+  LinkedIn,
+} from "@material-ui/icons";
+import avatar from "../images/kisspng.png";
+
+//Create css styles
+const useStyles = makeStyles((theme) => ({
+  menuSliderContainer: {
+    width: 250,
+    background: "#ccc",
+    height: "100%",
+  },
+  avatar: {
+    display: "block",
+    margin: "0.5rem auto",
+    width: theme.spacing(13),
+    height: theme.spacing(13),
+  },
+  listItem: {
+    color: "#e3f2fd",
+  },
+}));
+
+const menuItems = [
+  {
+    listIcon: <Home />,
+    listText: "Home",
+  },
+  {
+    listIcon: <AssignmentInd />,
+    listText: "Resume",
+  },
+  {
+    listIcon: <Apps />,
+    listText: "Portfolio",
+  },
+  {
+    listIcon: <ContactMail />,
+    listText: "Contacts",
+  },
+  {
+    listIcon: <GitHub />,
+    listText: "Github",
+  },
+  {
+    listIcon: <LinkedIn />,
+    listText: "Linkedin",
+  },
+];
+
+const Navbar = () => {
+  //Use hooks for state
+  const [state, setState] = useState({
+    right: false,
+  });
+  //Toogler methode
+  const toggleSlider = (slider, open) => () => {
+    setState({ ...state, [slider]: open });
+  };
+  const classes = useStyles();
+
+  const sideList = (slider) => (
+    <Box
+      component="div"
+      className={classes.menuSliderContainer}
+      onClick={toggleSlider(slider, false)}
+    >
+      <Avatar className={classes.avatar} src={avatar} alt="avatar image" />
+      <Divider />
+      <List>
+        {menuItems.map((lsItem, key) => (
+          <ListItem button>
+            <ListItemIcon className={classes.listItem}>
+              {lsItem.listIcon}
+            </ListItemIcon>
+            <ListItemText
+              className={classes.listItem}
+              primary={lsItem.listText}
+            />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <Fragment>
+      <Box component="nav">
+        <AppBar position="static" style={{ background: "#7986cb" }}>
+          <Toolbar>
+            <IconButton onClick={toggleSlider("right", true)}>
+              <Menu style={{ color: "#3d5afe" }} />
+            </IconButton>
+            <Typography variant="h5" style={{}}>
+              BarthDev
+            </Typography>
+            <MobilRightMenuSlider
+              anchor="right"
+              open={state.right}
+              onClose={toggleSlider("right", false)}
+            >
+              {sideList("right")}
+            </MobilRightMenuSlider>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </Fragment>
+  );
+};
+
+export default Navbar;
