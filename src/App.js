76851a8148c2f42ca0d433364223bf6b3e21d13c
barthelemy de violet <barthelemy.fornier-de-violet@epitech.eu>
2020-06-11 11:03:28 +0200

    add portfolio componenet for share project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Fragment } from "react";
 import { Route } from "react-router-dom";
-import Home from "./component/Index";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Home from "./component/Index";
 import About from "./component/About";
+import Portfolio from "./component/Portfolio";
 
 import "./App.css";
 
@@ -12,6 +13,7 @@ function App() {
       <CssBaseline />
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/portfolio" component={Portfolio} />
     </Fragment>
   );
 }
---
+++ src/component/Portfolio.js
@@ -0,0 +1,160 @@
+import React, { Fragment } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Box,
+  Grid,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import Navbar from "./Navbar";
+import project1 from "../images/github-finder.jpg";
+import project2 from "../images/games.jpg";
+
+const useStyles = makeStyles({
+  mainContainer: {
+    background: "#233",
+    height: "100%",
+  },
+  cardContainer: {
+    maxWidth: 345,
+    margin: "5rem auto",
+  },
+});
+
+const Portfolio = () => {
+  const classes = useStyles();
+  return (
+    <Box component="div" className={classes.mainContainer}>
+      <Navbar />
+      <Grid container justify="center">
+        {/* {Project-1} */}
+        <Grid item xs={12} sm={8} md={6}>
+          <Card className={classes.cardContainer}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                alt="project 1"
+                height="140"
+                image={project1}
+              ></CardMedia>
+              <CardContent>
+                <Typography gutterBottom variant="h5">
+                  Github Finder
+                </Typography>
+                <Typography component="p" variant="body2" color="textSecondary">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                  voluptates culpa assumenda porro, perferendis nostrum!
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+                <Button size="small" color="secondary">
+                  Live Demo
+                </Button>
+              </CardActions>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        {/* {Project-2} */}
+        <Grid item xs={12} sm={8} md={6}>
+          <Card className={classes.cardContainer}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                alt="project 2"
+                height="140"
+                image={project2}
+              ></CardMedia>
+              <CardContent>
+                <Typography gutterBottom variant="h5">
+                  Mini Game
+                </Typography>
+                <Typography component="p" variant="body2" color="textSecondary">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                  voluptates culpa assumenda porro, perferendis nostrum!
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+                <Button size="small" color="secondary">
+                  Live Demo
+                </Button>
+              </CardActions>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        {/* {Project-3} */}
+        <Grid item xs={12} sm={8} md={6}>
+          <Card className={classes.cardContainer}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                alt="project 3"
+                height="140"
+                image={project2}
+              ></CardMedia>
+              <CardContent>
+                <Typography gutterBottom variant="h5">
+                  project 3
+                </Typography>
+                <Typography component="p" variant="body2" color="textSecondary">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                  voluptates culpa assumenda porro, perferendis nostrum!
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+                <Button size="small" color="secondary">
+                  Live Demo
+                </Button>
+              </CardActions>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        {/* {Project-4} */}
+        <Grid item xs={12} sm={8} md={6}>
+          <Card className={classes.cardContainer}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                alt="project 4"
+                height="140"
+                image={project2}
+              ></CardMedia>
+              <CardContent>
+                <Typography gutterBottom variant="h5">
+                  Project 4
+                </Typography>
+                <Typography component="p" variant="body2" color="textSecondary">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                  voluptates culpa assumenda porro, perferendis nostrum!
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+                <Button size="small" color="secondary">
+                  Live Demo
+                </Button>
+              </CardActions>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Portfolio;
